84cd18ff3394276954ff79cc6874c625b1eebf6f
alexvegadev <alexvegadev@gmail.com>
2022-08-07 01:24:16 -0300

    Fix logger message display

--- internal/platform/log/logger.go
+++ internal/platform/log/logger.go
@@ -19,39 +19,39 @@ func init() {
 }
 
 func Info(msg ...interface{}) {
-	log.Info(msg)
+	log.Info(msg...)
 }
 
 func Infof(format string, args ...interface{}) {
-	log.Infof(format, args)
+	log.Infof(format, args...)
 }
 
 func Infoln(msg ...interface{}) {
-	log.Infoln(msg)
+	log.Infoln(msg...)
 }
 
 func Warn(args ...interface{}) {
-	log.Warn(args)
+	log.Warn(args...)
 }
 
 func Warnf(format string, args ...interface{}) {
-	log.Warnf(format, args)
+	log.Warnf(format, args...)
 }
 
 func Warnln(msg ...interface{}) {
-	log.Warnln(msg)
+	log.Warnln(msg...)
 }
 
 func Error(args ...interface{}) {
-	log.Error(args)
+	log.Error(args...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	log.Errorf(format, args)
+	log.Errorf(format, args...)
 }
 
 func Errorln(msg ...interface{}) {
-	log.Errorln(msg)
+	log.Errorln(msg...)
 }
 
 func GetOut() io.Writer {
